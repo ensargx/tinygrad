6e206a302f19c9f103b0fd7295580b9404d2beea
Ensar Gök <116498667+ensargx@users.noreply.github.com>
2026-04-13 19:49:55 +0300

    feat: add tanh and relu functions.
    also removed unnecesssery  function

--- tinynn/main.py
+++ tinynn/main.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+import math
 
 class Number:
     def __init__(self, val, parents: tuple[Number, ...] = (), *, requires_grad = True):
@@ -14,7 +15,8 @@ class Number:
     def __add__(self, other):
         other = other if isinstance(other, Number) else Number(other, requires_grad=False)
         val = self.val + other.val
-        new = self._create_child(other, val)
+        req_grad = self.requires_grad or other.requires_grad
+        new = Number(val, (self, other), requires_grad=req_grad)
 
         def _backprop():
             self._grad += new._grad
@@ -26,7 +28,8 @@ class Number:
     def __sub__(self, other):
         other = other if isinstance(other, Number) else Number(other)
         val = self.val - other.val
-        new = self._create_child(other, val)
+        req_grad = self.requires_grad or other.requires_grad
+        new = Number(val, (self, other), requires_grad=req_grad)
 
         def _backprop():
             self._grad += new._grad
@@ -47,7 +50,8 @@ class Number:
     def __mul__(self, other):
         other = other if isinstance(other, Number) else Number(other)
         val = self.val * other.val
-        new = self._create_child(other, val)
+        req_grad = self.requires_grad or other.requires_grad
+        new = Number(val, (self, other), requires_grad=req_grad)
 
         def _backprop():
             self._grad += other.val * new._grad
@@ -56,10 +60,30 @@ class Number:
 
         return new
 
+    def tanh(self):
+        val = math.tanh(self.val)
+        new = Number(val, (self,), requires_grad=self.requires_grad)
+
+        def _backprop():
+            self._grad += (1 - val**2) * new._grad
+        new._backprop = _backprop
+
+        return new
+
+    def relu(self):
+        val = self.val if self.val > 0 else 0
+        new = Number(val, (self,), requires_grad=self.requires_grad)
+
+        def _backprop():
+            self._grad += new._grad * (1 if new.val > 0 else 0)
+        new._backprop = _backprop
+
+        return new
+
     def __pow__(self, other):
         assert isinstance(other, (int, float)), "Only numbers"
         val = self.val ** other
-        new = Number(val, (self,), requires_grad=True)
+        new = Number(val, (self,), requires_grad=self.requires_grad)
 
         def _backprop():
             # n * x^(n-1)
@@ -103,11 +127,6 @@ class Number:
 
         for node in order:
             apply(node, *args, **kwargs)
-
-    def _create_child(self, other, val) -> Number:
-        other = other if isinstance(other, Number) else Number(other, requires_grad=False)
-        req_grad = self.requires_grad or other.requires_grad
-        return Number(val, (self, other), requires_grad=req_grad)
 
 def main():
     x = Number(0)
